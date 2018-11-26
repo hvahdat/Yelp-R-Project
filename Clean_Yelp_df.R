4ac784b224d3a56f5e774960c30699b266be217c
Clarisse Chia <clarisse-chia@uiowa.edu>
2018-11-26 10:21:21 -0600

    Add files via upload

--- Clean_Yelp_df.R
+++ Clean_Yelp_df.R
@@ -1,33 +1,33 @@
-# Load the package
+# Clear workspace
+rm(list = ls())
 
+# Load packages
 suppressPackageStartupMessages(library(dplyr)) #This will help us do simple manipulations
 suppressPackageStartupMessages(library(jsonlite)) #This will allow us to handle JSON data
 suppressPackageStartupMessages(library(tibble)) #This will give us a df that is friendly with dplyr
 suppressPackageStartupMessages(library(stringr)) #Use this to filter
+suppressPackageStartupMessages(library(tm)) #This will help us with text mining
 
-
-
-#bring in yelp json
+# Import in yelp in json file
 yelp <- stream_in(file("yelp_academic_dataset_business.json"))
 
-#flatten yelp file
+# Flatten yelp file
 yelp_flat <- flatten(yelp)
 
-
-#create tbl that can function with dplyr
+# Create tbl that can function with dplyr
 yelp_tbl <- as_data_frame(yelp_flat)
 
-
-#How to remove attributes. and hours. in front of colnames
+# Replace "attributes." and "hours." in front of colnames
+# with "a." and "h." respectively
 colnames(yelp_tbl) <- gsub("attributes.","a.",paste(colnames(yelp_tbl)))
 colnames(yelp_tbl) <- gsub("hours.","h.", paste(colnames(yelp_tbl)))
 
-#Export CSV
+# Export clean dataset into csv file
 write.csv(yelp_tbl, "yelp_df.csv", row.names = FALSE)
 
 
 
-              
+
 
 
 
